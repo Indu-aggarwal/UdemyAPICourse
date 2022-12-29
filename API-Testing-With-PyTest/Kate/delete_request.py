95e9e7914f1da4203e6505ca6df1d2b7de48b37e
ivanovakatya <katyaivanova0799@gmail.com>
2022-12-29 12:04:40 +0000

    Refactor delete file

--- API-Testing-With-PyTest/Kate/delete_request.py
+++ API-Testing-With-PyTest/Kate/delete_request.py
@@ -4,18 +4,29 @@ from decouple import config
 
 base_url = config('DOMAIN') + "/students"
 
-response = requests.get(base_url)
-json_response = json.loads(response.content)
+def delete_lastId_student_data(url):
+        response = requests.get(url)
+        json_response = json.loads(response.content)
+        response_array = []
+        for i in json_response:
+                response_array.append(i["id"])
 
-response_array = []
-for i in json_response:
-        response_array.append(i["id"])
+        if len(response_array) != 0:
+                last_element = response_array[- 1]
+                endpoint = url + "/" + str(last_element)
+                new_response = requests.delete(endpoint)
+                assert new_response.status_code == 200
+        else:
+                print("Nothing to delete.")
 
-if len(response_array) != 0:
-        last_element = response_array[- 1]
-        endpoint = base_url + "/" + str(last_element)
-        response = requests.delete(endpoint)
-        assert response.status_code == 200
-else:
-        print("Nothing to delete.")
+
+delete_lastId_student_data(base_url)
+
+def delete_randomId_student_data(url, studentId):
+        endpoint = url + "/" + str(studentId)
+        updated_response = requests.delete(endpoint)
+        assert updated_response.status_code == 200
+
+dataId = 5
+# delete_randomId_student_data(base_url, dataId)
 
