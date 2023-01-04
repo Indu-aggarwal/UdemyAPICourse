4c3fb403589daea0762af535aab3168d155679df
ivanovakatya <katyaivanova0799@gmail.com>
2023-01-04 10:31:47 +0000

    Latest chages

--- API-Testing-With-PyTest/Kate/PytestRestAPI/test_get_specific_user_id.py
+++ API-Testing-With-PyTest/Kate/PytestRestAPI/test_get_specific_user_id.py
@@ -5,5 +5,5 @@ from decouple import config
 def test_get_user_data():
         response = requests.get(config('DOMAIN') + "/students/2")
         json_response = json.loads(response.content)
-        assert response.status_code == 200
+        
         assert json_response["id"] == 2
